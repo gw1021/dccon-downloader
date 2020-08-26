c05671cdcdead26cb6296da2fb117ee991e65e09
Gwonwoo Choi <gw.choi@wonwoo.com>
2020-08-26 11:18:33 +0900

    HTTP ➡ HTTPS, Remove TQDM

--- python/app.py
+++ python/app.py
@@ -1,39 +1,53 @@
-from tqdm import tqdm
 import requests
 import os
 
-dccon_id = input("input dccon id :")
+PROTOCOL_PREFIX = 'https://'
 
-header_for_getCookie = {'X-Requested-With':'XMLHttpRequest'}
-header_for_imageServer = {'Referer': 'http://dccon.dcinside.com/'}
-package_detail_url = 'http://dccon.dcinside.com/index/package_detail'
-download_url = 'http://dcimg5.dcinside.com/dccon.php?no='
-# Open session
-s = requests.Session()
-# Get Cookie (ci_c)
-r = s.get(package_detail_url, headers=header_for_getCookie)
-# Get Json (ci_c to ci_t)
-req = s.post(package_detail_url, headers=header_for_getCookie, data={'ci_t':r.cookies['ci_c'], 'package_idx':dccon_id})
 
-json_data = req.json()
+def download(num=None):
+    con_id = num
+    if num is None:
+        con_id = input("DCCON NUM :")
+    if con_id == '':
+        exit()
 
-default_dir = os.path.dirname(os.path.abspath(__file__))
-download_path = os.path.join(default_dir, json_data['info']['title'])
+    loading_chars = ['|', '/', '-', '\\']
+    package_url = f'{PROTOCOL_PREFIX}dccon.dcinside.com/index/package_detail'
+    download_url = f'{PROTOCOL_PREFIX}dcimg5.dcinside.com/dccon.php?no='
+    # Open session
+    s = requests.Session()
+    # Get Cookie (ci_c)
+    r = s.get(package_url,
+              headers={'X-Requested-With': 'XMLHttpRequest'})
+    # Get Json (ci_c to ci_t)
+    req = s.post(package_url,
+                 headers={'X-Requested-With': 'XMLHttpRequest'},
+                 data={'ci_t': r.cookies['ci_c'], 'package_idx': con_id})
 
-try:
-	os.makedirs(download_path)
-except Exception as e:
-	print(e)
-else:
-	for item in tqdm(json_data['detail']):
-		filename = item['idx']+'.'+item['ext']
-		image = s.get(download_url+item['path'], headers=header_for_imageServer)
-		with open(os.path.join(download_path, filename), 'wb') as fd:
-			for chunk in image.iter_content(chunk_size=128):
-				fd.write(chunk)
-			fd.close()
-	# Session Close
-	s.close()
-	print('Job [', dccon_id, '] Complete!! ^^7')
+    json_data = req.json()
 
-os.system("pause")
+    default_dir = os.path.dirname(os.path.abspath(__file__))
+    download_path = os.path.join(default_dir, json_data['info']['title'])
+
+    try:
+        os.makedirs(download_path)
+    except Exception as e:
+        print(e)
+    else:
+        loading_cnt = 0
+
+        for item in json_data['detail']:
+            print(f'\rLoading... ({loading_chars[loading_cnt]})', end='')
+            loading_cnt = (loading_cnt + 1) % 4
+            filename = item['idx'] + '.' + item['ext']
+            image = s.get(download_url + item['path'],
+                          headers={'Referer': f'{PROTOCOL_PREFIX}dccon.dcinside.com/'})
+            with open(os.path.join(download_path, filename), 'wb') as fd:
+                for chunk in image.iter_content(chunk_size=128):
+                    fd.write(chunk)
+                fd.close()
+        s.close()
+
+
+if __name__ == "__main__":
+    download()
